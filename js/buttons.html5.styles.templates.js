adf8d7261b5d1f0abeca2e7a5fd7dd695905883e
Paul Jones <info@jessicajones.co.nz>
2020-06-26 20:06:57 +1200

    0.9.0

--- js/buttons.html5.styles.templates.js
+++ js/buttons.html5.styles.templates.js
@@ -1,7 +1,7 @@
 /**
  * Style templates for html5.styles
  *
- * @version: 0.8.1
+ * @version: 0.9.0
  * @description Easy templates for 'excelStyles'
  * @file buttons.html5.styles.templates.js
  * @copyright © 2020 Beyond the Box Creative
